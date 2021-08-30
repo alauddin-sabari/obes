14037a042960c073d008cbd8cb29b1049209dd92
alauddinsabari1 <aladinsabari@gmail.com>
2021-08-30 22:39:17 +0600

    edited

---
+++ design/1.js
@@ -0,0 +1,27 @@
+demo.style.display = 'none';
+
+function toggleHide() {
+    if (demo.style.display != 'none') {
+        demo.style.display = 'block';
+    } else {
+        demo.style.display = 'block';
+
+    }
+}
+demos.style.display = 'none';
+
+function toggleHide() {
+    if (demos.style.display != 'none') {
+        demos.style.display = 'block';
+    } else {
+        demos.style.display = 'block';
+
+    }
+}
+
+
+// var frame = document.querySelector("iframe");
+// header = frame.contentDocument.getElementByIde("menubar");
+// header.style.color = red;
+// footer = frame.contentDocument.querySelector("footer");
+// footer.remove();
